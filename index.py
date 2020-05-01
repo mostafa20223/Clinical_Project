32540b184efc62c85a53fff3d31a2615a035ff7f
mostafa20223 <mustafayehia4@gmail.com>
2020-05-02 00:50:21 +0200

    Add

--- index.py
+++ index.py
@@ -60,13 +60,18 @@ class MainApp(QMainWindow, ui):
         self.more_info_tab.clicked.connect(self.open_info_tab)
         self.pushButton_8.clicked.connect(self.Add_new_book)
         self.add_cat_btn.clicked.connect(self.Add_category)
-        self.pushButton_17.clicked.connect(self.Dark_orange_theme)
-        self.pushButton_18.clicked.connect(self.Dark_blue_Theme)
-        self.pushButton_20.clicked.connect(self.Dark_gray_theme)
-        self.pushButton_19.clicked.connect(self.qdark_theme)
+        self.view_cat_btn.clicked.connect(self.show_category)
+        self.dark_orange.clicked.connect(self.Dark_orange_theme)
+        self.dark_blue.clicked.connect(self.Dark_blue_Theme)
+        self.dark_grey.clicked.connect(self.Dark_gray_theme)
+        self.dark.clicked.connect(self.qdark_theme)
         self.add_user_btn.clicked.connect(self.Add_new_user)
         self.user_login_btn.clicked.connect(self.login)
         self.edit_user_btn.clicked.connect(self.Edit_user)
+        self.add_eng_btn.clicked.connect(self.Add_engineer)
+        self.view_eng_btn.clicked.connect(self.show_engineer)
+        self.add_vendor_btn.clicked.connect(self.Add_vendor)
+        self.view_vendor_btn.clicked.connect(self.show_vendor)
 
     def Show_Themes(self):
         self.groupBox_4.show()
@@ -166,7 +171,6 @@ class MainApp(QMainWindow, ui):
         self.cur = self.db.cursor()
         category_name = self.cat_name.text()
         category_floor = self.cat_floor.text()
-
         self.cur.execute(''' INSERT INTO category (name, floor) VALUES (%s, %s) '''
                          , (category_name, category_floor))
         self.db.commit()
@@ -178,25 +182,36 @@ class MainApp(QMainWindow, ui):
     def show_category(self):
         self.db = mysql.connector.connect(host = 'localhost', user = 'root', password = 'DARSH1999', db = 'cmms')
         self.cur = self.db.cursor()
-        self.cur.execute(''' SELECT name FROM category ''')
+        self.cur.execute(''' SELECT * FROM category ''')
         data = self.cur.fetchall()
-        #print(data)
+        print(data)
+
+#        if data:
+#            self.category_table.setRowCount(0)
+#            self.category_table.insertRow(0)
+#            for i in enumerate(data):
+#                j = 0
+#                while j < len(data):
+#                    self.category_table.setItem(j, i, QTableWidgetItem(str(data[j][i])))
+#                    j += 1
+#                row_position = self.category_table.rowCount()
+#                self.category_table.insertRow(row_position)
 
         if data:
-            self.tableWidget_2.setRowCount(0)
-            self.tableWidget_2.insertRow(0)
+            self.category_table.setRowCount(0)
+            self.category_table.insertRow(0)
             for row, form in enumerate(data):
                 for column, item in enumerate(form):
-                    self.tableWidget_2.setItem(row, column, QTableWidgetItem(str(item)))
+                    self.category_table.setItem(row, column, QTableWidgetItem(str(item)))
+                    #self.category_table.setItem(row + 1, column + 1, QTableWidgetItem(str(item[row + 1][column + 1])))
                     column += 1
-                row_position = self.tableWidget_2.rowCount()
-                self.tableWidget_2.insertRow(row_position)
+                row_position = self.category_table.rowCount()
+                self.category_table.insertRow(row_position)
 
     # Show Category in UI
     def show_category_combobox(self):
         self.db = mysql.connector.connect(host = 'localhost', user = 'root', password = 'DARSH1999', db = 'cmms')
         self.cur = self.db.cursor()
-
         self.cur.execute(''' SELECT name FROM category ''')
         data = self.cur.fetchall()
         print(data)
@@ -212,7 +227,6 @@ class MainApp(QMainWindow, ui):
         eng_name = self.cat_floor.text()
         eng_phone = self.cat_name.text()
         eng_email = self.cat_floor.text()
-
         self.cur.execute(''' INSERT INTO engineer (ssn, eng name, eng phone, eng email) VALUES (%s, %s, %s, %s) '''
                          , (ssn, eng_name, eng_phone, eng_email))
         self.db.commit()
@@ -221,9 +235,9 @@ class MainApp(QMainWindow, ui):
         self.cat_floor.setText(' ')
         self.cat_name.setText(' ')
         self.cat_floor.setText(' ')
-        self.show_engineer()
+        self.show_equipment()
 
-    def show_engineer(self):
+    def show_equipment(self):
         self.db = mysql.connector.connect(host = 'localhost', user = 'root', password = 'DARSH1999', db = 'cmms')
         self.cur = self.db.cursor()
         self.cur.execute(''' SELECT ssn FROM engineer ''')
@@ -244,55 +258,91 @@ class MainApp(QMainWindow, ui):
     def Add_engineer(self):
         self.db = mysql.connector.connect(host = 'localhost', user = 'root', password = 'DARSH1999', db = 'cmms')
         self.cur = self.db.cursor()
-        ssn = self.cat_name.text()
-        eng_name = self.cat_floor.text()
-        eng_phone = self.cat_name.text()
-        eng_email = self.cat_floor.text()
+        ssn = self.eng_ssn.text()
+        eng_name = self.eng_name.text()
+        eng_phone = self.eng_phone.text()
+        eng_email = self.eng_mail.text()
 
-        self.cur.execute(''' INSERT INTO engineer (ssn, eng name, eng phone, eng email) VALUES (%s, %s, %s, %s) '''
+        self.cur.execute(''' INSERT INTO engineer (ssn, eng_name, eng_phone, eng_email) VALUES (%s, %s, %s, %s) '''
                          , (ssn, eng_name, eng_phone, eng_email))
         self.db.commit()
         self.statusBar().showMessage('New Engineer Added')
-        self.cat_name.setText(' ')
-        self.cat_floor.setText(' ')
-        self.cat_name.setText(' ')
-        self.cat_floor.setText(' ')
+        self.eng_ssn.setText(' ')
+        self.eng_name.setText(' ')
+        self.eng_mail.setText(' ')
+        self.eng_phone.setText(' ')
         self.show_engineer()
 
     def show_engineer(self):
         self.db = mysql.connector.connect(host = 'localhost', user = 'root', password = 'DARSH1999', db = 'cmms')
-        self.cur = self.db.cursor()
-        self.cur.execute(''' SELECT ssn FROM engineer ''')
-        data = self.cur.fetchall()
+        self.cur0 = self.db.cursor()
+        self.cur1 = self.db.cursor()
+        self.cur2 = self.db.cursor()
+        self.cur3 = self.db.cursor()
+        self.cur0.execute(''' SELECT ssn FROM engineer ''')
+        self.cur1.execute(''' SELECT eng_name FROM engineer ''')
+        self.cur2.execute(''' SELECT eng_email FROM engineer ''')
+        self.cur3.execute(''' SELECT eng_phone FROM engineer ''')
+        data0 = self.cur0.fetchall()
+        data1 = self.cur1.fetchall()
+        data2 = self.cur2.fetchall()
+        data3 = self.cur3.fetchall()
         #print(data)
 
-        if data:
-            self.tableWidget_2.setRowCount(0)
-            self.tableWidget_2.insertRow(0)
-            for row, form in enumerate(data):
+        if data0:
+            self.eng_table.setRowCount(0)
+            self.eng_table.insertRow(0)
+            for row, form in enumerate(data0):
                 for column, item in enumerate(form):
-                    self.tableWidget_2.setItem(row, column, QTableWidgetItem(str(item)))
+                    self.eng_table.setItem(row, column, QTableWidgetItem(str(item)))
                     column += 1
-                row_position = self.tableWidget_2.rowCount()
-                self.tableWidget_2.insertRow(row_position) 
+                row_position = self.eng_table.rowCount()
+                self.eng_table.insertRow(row_position) 
+        if data1:
+            self.eng_table.setRowCount(1)
+            self.eng_table.insertRow(1)
+            for row, form in enumerate(data1):
+                for column, item in enumerate(form):
+                    self.eng_table.setItem(row, column, QTableWidgetItem(str(item)))
+                    column += 1
+                row_position = self.eng_table.rowCount()
+                self.eng_table.insertRow(row_position) 
+        if data2:
+            self.eng_table.setRowCount(2)
+            self.eng_table.insertRow(2)
+            for row, form in enumerate(data2):
+                for column, item in enumerate(form):
+                    self.eng_table.setItem(row, column, QTableWidgetItem(str(item)))
+                    column += 1
+                row_position = self.eng_table.rowCount()
+                self.eng_table.insertRow(row_position) 
+        if data3:
+            self.eng_table.setRowCount(3)
+            self.eng_table.insertRow(3)
+            for row, form in enumerate(data3):
+                for column, item in enumerate(form):
+                    self.eng_table.setItem(row, column, QTableWidgetItem(str(item)))
+                    column += 1
+                row_position = self.eng_table.rowCount()
+                self.eng_table.insertRow(row_position) 
 
     # Vendor
     def Add_vendor(self):
         self.db = mysql.connector.connect(host = 'localhost', user = 'root', password = 'DARSH1999', db = 'cmms')
         self.cur = self.db.cursor()
-        vendor_name = self.cat_name.text()
-        vendor_ssn = self.cat_floor.text()
-        vendor_phone = self.cat_floor.text()
-        vendor_email = self.cat_floor.text()
+        vendor_name = self.vendor_name.text()
+        vendor_ssn = self.vendor_ssn.text()
+        vendor_phone = self.vendor_phone.text()
+        vendor_email = self.vendor_mail.text()
 
         self.cur.execute(''' INSERT INTO vendor (vendor ssn, vendor name, vendor phone, vendor email) VALUES (%s, %s, %s, %s) '''
                          , (vendor_ssn, vendor_name, vendor_phone, vendor_email))
         self.db.commit()
         self.statusBar().showMessage('New Vendor Added')
-        self.cat_name.setText(' ')
-        self.cat_floor.setText(' ')
-        self.cat_name.setText(' ')
-        self.cat_floor.setText(' ')
+        self.vendor_name.setText(' ')
+        self.vendor_ssn.setText(' ')
+        self.vendor_phone.setText(' ')
+        self.vendor_mail.setText(' ')
         self.show_vendor()
 
     def show_vendor(self):
